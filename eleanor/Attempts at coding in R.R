069f2ccd5c3a7afe5035e78087bd62ad8f66d4b7
ERWoodhead <32897082+ERWoodhead@users.noreply.github.com>
2017-11-06 21:50:56 +0000

    More code for the code gods
    Figured out how to plot the mean score of each university on a single graph, dunno if it'll be of any use but it exists now.
    Could probably plot on some SE bars if I get round to it

--- eleanor/Attempts at coding in R.R
+++ eleanor/Attempts at coding in R.R
@@ -15,3 +15,14 @@ library(plyr)
 ddply(OnlyOverall, .(SortOrder), summarize,  FourStarMean=mean(FourStar), ThreeStarMean=mean(ThreeStar), TwoStarMean=mean(TwoStar), OneStarMean=mean(OneStar), UnclassifiedMean=mean(Unclassified))
 
 Original code from: https://stackoverflow.com/questions/21982987/mean-per-group-in-a-data-frame
+
+
+# For plotting the mean scores of each university on one graph.
+# Letstryit is the Mean_Overalls dataset.
+plot(factor(letstryit$Institution),letstryit$FourStarMean,ylab="% Mark",xlab="University", ylim=c(0,80),main="Plot showing the mean scores for each university")
+points(factor(letstryit$Institution),letstryit$ThreeStarMean, pch= 15,col="red", cex=0.35)
+points(factor(letstryit$Institution),letstryit$TwoStarMean, pch= 15,col="darkgreen", cex=0.35)
+points(factor(letstryit$Institution),letstryit$OneStarMean, pch= 15,col="blue", cex=0.35)
+points(factor(letstryit$Institution),letstryit$UnclassifiedMean, pch= 15,col="deeppink", cex=0.35)
+legend("topright", legend=c("Four Star Mean","Three Star Mean", "Two Star Mean","One Star mean","Unclassifed Mean"), col=c("black","red", "darkgreen","blue","deeppink"), pch=15, cex=0.5, horiz=TRUE)
+
