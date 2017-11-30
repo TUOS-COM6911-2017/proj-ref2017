ab62c744e043084dd5af7630192ce8b3e91de87f
ERWoodhead <32897082+ERWoodhead@users.noreply.github.com>
2017-11-30 21:04:02 +0000

    A fancy barchart and my chicken scratch of coding
    whoo good luck reading the second one
    
    Barchart is just showing the distribution of the scores across the different regions.
    it was a pain to code.

--- eleanor/Attempts at coding in R.R
+++ eleanor/Attempts at coding in R.R
@@ -1,5 +1,5 @@
 # To make a dataset containing only the overall scores
-OnlyOverall=subset(Initial_Test, Profile!="Outputs") # Initial_Test is the file from REF2014
+OnlyOverall=subset(Initial_Test, Profile=="Outputs") # Initial_Test is the file from REF2014
 OnlyOverall=subset(OnlyOverall, Profile!="Impact")
 OnlyOverall=subset(OnlyOverall, Profile!="Environment")
 
@@ -17,6 +17,7 @@ ddply(OnlyOverall, .(SortOrder), summarize,  FourStarMean=mean(FourStar), ThreeS
 Original code from: https://stackoverflow.com/questions/21982987/mean-per-group-in-a-data-frame
 
 
+
 # For plotting the mean scores of each university on one graph.
 # Letstryit is the Mean_Overalls dataset.
 plot(factor(letstryit$Institution),letstryit$FourStarMean,ylab="% Mark",xlab="University", ylim=c(0,80),main="Plot showing the mean scores for each university")
@@ -26,3 +27,20 @@ points(factor(letstryit$Institution),letstryit$OneStarMean, pch= 15,col="blue", 
 points(factor(letstryit$Institution),letstryit$UnclassifiedMean, pch= 15,col="deeppink", cex=0.35)
 legend("topright", legend=c("Four Star Mean","Three Star Mean", "Two Star Mean","One Star mean","Unclassifed Mean"), col=c("black","red", "darkgreen","blue","deeppink"), pch=15, cex=0.5, horiz=TRUE)
 
+
+
+# original code from https://stackoverflow.com/questions/14544124/grouped-bar-graph-using-barplot
+mymat <- t(Meanbyregion[-1])
+
+colnames(mymat) <- Meanbyregion[, 1]
+barplot(mymat,col=c("plum1","plum","plum2","plum3","plum4"),xlab="Region",ylab="%")
+legend(0.5,120,legend=c("4* Grade", "3* Grade","2* Grade","1* Grade", "Unclassified Grade"," "),text.width=c(1.7,1.7,1.7,1.7,1.7,1.7),col=c("plum1","plum","plum2","plum3","plum4","white"),pch=15,horiz=TRUE, cex=0.7)
+
+op <- par(mar=c(7.8,4,1.55,2))
+barplot(mymat,col=c("plum1","plum","plum2","plum3","plum4"),xlab="",ylab="%",las=2,cex.names=0.65)
+legend(0.5,118,legend=c("4* Grade", "3* Grade","2* Grade","1* Grade", "Unclassified Grade"," "),text.width=c(1.7,1.7,1.7,1.7,1.7,1.7),col=c("plum1","plum","plum2","plum3","plum4","white"),pch=15,horiz=TRUE, cex=0.7)
+mtext("Region", side=1, line=6.5)
+
+
+barplot(mymat,col=c("plum1","plum","plum2","plum3","plum4"),xlab="",ylab="%",las=2,cex.names=0.7)
+mtext("Region", side=1, line=6.5)
